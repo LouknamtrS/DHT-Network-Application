a43ea4a591b687d20d3508fb647a38ffd507a55a
Sasima Phanta <jassm10phanta@gmail.com>
2024-12-17 16:41:42 +0700

    navbar and homepage
    navbar component and homepage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
+import Home from "./Home.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center text-red-500">
+      {/* <h1 className="text-3xl font-bold underline text-center text-red-500">
         Hello world!
-      </h1>
+      </h1> */}
+      <Home/>
     </>
   );
 }
---
+++ Client/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "./component/Navbar.jsx";
+
+
+function Home() {
+    return(
+        <>
+            <Navbar/>
+        </>
+    )
+}
+
+export default Home;
